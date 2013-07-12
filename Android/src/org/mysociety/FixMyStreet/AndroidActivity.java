25e3a0c94551b0faac3f16e0598e6f4bdffcf7d8
Struan Donald <struan@exo.org.uk>
2013-07-12 09:47:56 +0100

    instead of using js to inject the correct cordova js file in to index.html restructure things so that the common files are a level down and the platofrm specific ones are directly placed in the relevant project. This both makes for less fuss and also avoids the error with Android < v3 instantiating cordova twice.
    Note that the iOS common assets are included by a build script rather
    than a symlink as symlinking doesn't seem to agree with Xcode

--- Android/src/org/mysociety/FixMyStreet/AndroidActivity.java
+++ Android/src/org/mysociety/FixMyStreet/AndroidActivity.java
@@ -8,6 +8,6 @@ public class AndroidActivity extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
+        super.loadUrl("file:///android_asset/www/src/index.html");
     }
 }
