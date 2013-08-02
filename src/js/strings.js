f7bc98fb391dbe7fceee1cdb7872a4d4c225c484
Struan Donald <struan@exo.org.uk>
2013-08-02 17:11:24 +0100

    Make message strings a bit more consistent
    Add full stop at the end of all messages
    try again -> try again later

--- src/js/strings.js
+++ src/js/strings.js
@@ -20,19 +20,19 @@
             password: 'Please enter a password'
         },
         strings: {
-            login_error: 'There was a problem logging you in. Please try later',
-            logout_error: 'There was a problem logging you out. Please try later',
+            login_error: 'There was a problem logging you in. Please try again later.',
+            logout_error: 'There was a problem logging you out. Please try again later.',
             login_details_error: 'There was a problem logging you in. Please check your email and password',
-            password_problem: 'There was a problem with your email/password combination. If you have forgotten your password, or do not have one, you can set one by returning to the email screen and selecting the set password option',
+            password_problem: 'There was a problem with your email/password combination. If you have forgotten your password, or do not have one, you can set one by returning to the email screen and selecting the set password option.',
             location_error: 'Location error',
-            location_problem: 'There was a problem looking up your location',
+            location_problem: 'There was a problem looking up your location.',
             multiple_locations: 'More than one location matched that name. Select one below or try entering street name and area, or a postcode.',
             sync_error: 'An error was encountered when submitting your report: ',
             unknown_sync_error: 'There was a problem submitting your report. Please try again later.',
-            report_send_error: 'There was a problem submitting your report. Please try again',
+            report_send_error: 'There was a problem submitting your report. Please try again.',
             missing_location: 'Please enter a location',
             location_check_failed: 'There was a problem checking we cover this location. Please try again later.',
-            category_extra_check_error: 'There was a problem checking if we have all the details we need. Please try again later',
+            category_extra_check_error: 'There was a problem checking if we have all the details we need. Please try again later.',
             locate_dismissed: 'Please search for a street name and area, or postcode.',
             geolocation_failed: 'Could not determine your location, please search for a street name and area, or postcode instead.',
             geolocation_denied: 'Could not access location services. Please check permissions.',
@@ -40,8 +40,8 @@
             required: 'required',
             invalid_email: 'Invalid email',
             invalid_report: 'Invalid report',
-            photo_failed: 'There was a problem taking your photo',
-            no_connection: 'No network connection available for submitting your report. Please try again later',
+            photo_failed: 'There was a problem taking your photo.',
+            no_connection: 'No network connection available for submitting your report. Please try again later.',
             more_details: 'More details'
         }
     });
--- templates/strings.js
+++ templates/strings.js
@@ -20,19 +20,19 @@
             password: '[% loc('Please enter a password') %]'
         },
         strings: {
-            login_error: '[% loc('There was a problem logging you in. Please try later') %]',
-            logout_error: '[% loc('There was a problem logging you out. Please try later') %]',
+            login_error: '[% loc('There was a problem logging you in. Please try again later.') %]',
+            logout_error: '[% loc('There was a problem logging you out. Please try again later.') %]',
             login_details_error: '[% loc('There was a problem logging you in. Please check your email and password') %]',
-            password_problem: '[% loc('There was a problem with your email/password combination. If you have forgotten your password, or do not have one, you can set one by returning to the email screen and selecting the set password option') %]',
+            password_problem: '[% loc('There was a problem with your email/password combination. If you have forgotten your password, or do not have one, you can set one by returning to the email screen and selecting the set password option.') %]',
             location_error: '[% loc('Location error') %]',
-            location_problem: '[% loc('There was a problem looking up your location') %]',
+            location_problem: '[% loc('There was a problem looking up your location.') %]',
             multiple_locations: '[% loc('More than one location matched that name. Select one below or try entering street name and area, or a postcode.') %]',
             sync_error: '[% loc('An error was encountered when submitting your report: ') %]',
             unknown_sync_error: '[% loc('There was a problem submitting your report. Please try again later.') %]',
-            report_send_error: '[% loc('There was a problem submitting your report. Please try again') %]',
+            report_send_error: '[% loc('There was a problem submitting your report. Please try again.') %]',
             missing_location: '[% loc('Please enter a location') %]',
             location_check_failed: '[% loc('There was a problem checking we cover this location. Please try again later.') %]',
-            category_extra_check_error: '[% loc('There was a problem checking if we have all the details we need. Please try again later') %]',
+            category_extra_check_error: '[% loc('There was a problem checking if we have all the details we need. Please try again later.') %]',
             locate_dismissed: '[% loc('Please search for a street name and area, or postcode.') %]',
             geolocation_failed: '[% loc('Could not determine your location, please search for a street name and area, or postcode instead.') %]',
             geolocation_denied: '[% loc('Could not access location services. Please check permissions.') %]',
@@ -40,8 +40,8 @@
             required: '[% loc('required') %]',
             invalid_email: '[% loc('Invalid email') %]',
             invalid_report: '[% loc('Invalid report') %]',
-            photo_failed: '[% loc('There was a problem taking your photo') %]',
-            no_connection: '[% loc('No network connection available for submitting your report. Please try again later') %]',
+            photo_failed: '[% loc('There was a problem taking your photo.') %]',
+            no_connection: '[% loc('No network connection available for submitting your report. Please try again later.') %]',
             more_details: '[% loc('More details') %]'
         }
     });
