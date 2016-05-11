0c7e11a18728eb5b6f4f0f07e82baf34a0f1d651
Matthew Somerville <matthew-github@dracos.co.uk>
2016-05-11 12:11:40 +0100

    Use OpenStreetMap tiles by default.
    Add a MAP_TYPE configuration option to choose what map tiles to use,
    defaulting to OpenStreetMap. This fixes the issue of people outside the
    UK getting a grey map.

--- www/js/map-OpenStreetMap.js
+++ www/js/map-OpenStreetMap.js
@@ -1,13 +1,11 @@
 function set_map_config(perm) {
-    var permalink_id;
-    if ($('#map_permalink').length) {
-        permalink_id = 'map_permalink';
-    }
     fixmystreet.controls = [
+        new OpenLayers.Control.Attribution(),
         new OpenLayers.Control.ArgParser(),
-        //new OpenLayers.Control.Navigation(),
+        new OpenLayers.Control.Navigation(),
         new OpenLayers.Control.PanZoomFMS({id: 'fms_pan_zoom' })
     ];
+    fixmystreet.map_type = OpenLayers.Layer.OSM.Mapnik;
 }
 
 // http://www.openstreetmap.org/openlayers/OpenStreetMap.js (added maxResolution)
@@ -58,13 +56,13 @@ OpenLayers.Layer.OSM.Mapnik = OpenLayers.Class(OpenLayers.Layer.OSM, {
      */
     initialize: function(name, options) {
         var url = [
-            "http://a.tile.openstreetmap.org/${z}/${x}/${y}.png",
-            "http://b.tile.openstreetmap.org/${z}/${x}/${y}.png",
-            "http://c.tile.openstreetmap.org/${z}/${x}/${y}.png"
+            "https://a.tile.openstreetmap.org/${z}/${x}/${y}.png",
+            "https://b.tile.openstreetmap.org/${z}/${x}/${y}.png",
+            "https://c.tile.openstreetmap.org/${z}/${x}/${y}.png"
         ];
         options = OpenLayers.Util.extend({
             /* Below line added to OSM's file in order to allow minimum zoom level */
-            maxResolution: 156543.0339/Math.pow(2, options.zoomOffset || 0),
+            maxResolution: 156543.03390625/Math.pow(2, options.zoomOffset || 0),
             numZoomLevels: 19,
             buffer: 0
         }, options);
@@ -73,34 +71,6 @@ OpenLayers.Layer.OSM.Mapnik = OpenLayers.Class(OpenLayers.Layer.OSM, {
     },
 
     CLASS_NAME: "OpenLayers.Layer.OSM.Mapnik"
-});
-
-/**
- * Class: OpenLayers.Layer.OSM.Osmarender
- *
- * Inherits from:
- *  - <OpenLayers.Layer.OSM>
- */
-OpenLayers.Layer.OSM.Osmarender = OpenLayers.Class(OpenLayers.Layer.OSM, {
-    /**
-     * Constructor: OpenLayers.Layer.OSM.Osmarender
-     *
-     * Parameters:
-     * name - {String}
-     * options - {Object} Hashtable of extra options to tag onto the layer
-     */
-    initialize: function(name, options) {
-        var url = [
-            "http://a.tah.openstreetmap.org/Tiles/tile/${z}/${x}/${y}.png",
-            "http://b.tah.openstreetmap.org/Tiles/tile/${z}/${x}/${y}.png",
-            "http://c.tah.openstreetmap.org/Tiles/tile/${z}/${x}/${y}.png"
-        ];
-        options = OpenLayers.Util.extend({ numZoomLevels: 18, buffer: 0 }, options);
-        var newArguments = [name, url, options];
-        OpenLayers.Layer.OSM.prototype.initialize.apply(this, newArguments);
-    },
-
-    CLASS_NAME: "OpenLayers.Layer.OSM.Osmarender"
 });
 
 /**
@@ -125,7 +95,7 @@ OpenLayers.Layer.OSM.CycleMap = OpenLayers.Class(OpenLayers.Layer.OSM, {
         ];
         options = OpenLayers.Util.extend({
             /* Below line added to OSM's file in order to allow minimum zoom level */
-            maxResolution: 156543.0339/Math.pow(2, options.zoomOffset || 0),
+            maxResolution: 156543.03390625/Math.pow(2, options.zoomOffset || 0),
             numZoomLevels: 19,
             buffer: 0
         }, options);
