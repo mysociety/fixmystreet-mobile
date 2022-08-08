b31f235b2629a443a5bd6b84dc849f2ab31a2e1c
Dave Arter <davea@mysociety.org>
2022-08-08 13:30:26 +0100

    Initial commit of PWABuilder wrapper

---
+++ README.md
@@ -0,0 +1,12 @@
+# FixMyStreet PWA iOS Wrapper
+
+An iOS app built with [PWABuilder](https://www.pwabuilder.com/) that wraps
+the FMS progressive web app for publishing on the iOS App Store.
+
+## Installation/Building
+
+You'll need Xcode and [CocoaPods](https://cocoapods.org) (`brew install cocoapods` should do the trick.)
+
+ 1. Run `pod install` from within the `src` directory
+ 1. `open FixMyStreet.xcworkspace`
+ 1. You should be able to build/run the app, though may need to tell Xcode to automatically manage code signing.
---
+++ src/FixMyStreet/Settings.swift
@@ -0,0 +1,30 @@
+import WebKit
+
+struct Cookie {
+    var name: String
+    var value: String
+}
+
+let gcmMessageIDKey = "00000000000" // update this with actual ID if using Firebase 
+
+// URL for first launch
+//let rootUrl = URL(string: "https://www.fixmystreet.com/?pwa")!
+let rootUrl = URL(string: "https://fixmystreet.eu.ngrok.io/?pwa")!
+
+// allowed origin is for what we are sticking to pwa domain
+// This should also appear in Info.plist
+//let allowedOrigins: [String] = ["www.fixmystreet.com"]
+let allowedOrigins: [String] = ["fixmystreet.eu.ngrok.io"]
+
+// auth origins will open in modal and show toolbar for back into the main origin.
+// These should also appear in Info.plist
+let authOrigins: [String] = []
+// allowedOrigins + authOrigins <= 10
+
+let platformCookie = Cookie(name: "app-platform", value: "iOS App Store")
+
+// UI options
+let displayMode = "standalone" // standalone / fullscreen.
+let adaptiveUIStyle = true     // iOS 15+ only. Change app theme on the fly to dark/light related to WebView background color.
+let overrideStatusBar = false   // iOS 13-14 only. if you don't support dark/light system theme.
+let statusBarTheme = "dark"    // dark / light, related to override option.
